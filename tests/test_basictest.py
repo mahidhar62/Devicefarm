75498b3afd15726677d82cc306866f9a2580e5d4
mahidhar62 <79515474+mahidhar62@users.noreply.github.com>
2021-02-23 20:11:46 +0530

    Create test_basictest.py

---
+++ tests/test_basictest.py
@@ -0,0 +1,35 @@
+import boto3, pytest
+import os
+import unittest
+from appium import webdriver
+import selenium
+from time import sleep
+
+# in your tests:
+# Set up the Device Farm client, get a driver URL:
+class DeviceFarmAppiumWebTests(unittest.TestCase):
+  def setup_method(self, method):
+    devicefarm_client = boto3.client("devicefarm", region_name="us-west-2")
+    testgrid_url_response = devicefarm_client.create_test_grid_url(
+      projectArn="arn:aws:devicefarm:us-west-2:503085204531:testgrid-project:9e6db8fd-737b-4a63-819b-028e112a0747",
+      expiresInSeconds=300)
+    desired_caps = {}
+    desired_caps['browserName'] = 'firefox'
+    self.driver = webdriver.Remote(testgrid_url_response["url"], desired_caps)
+
+
+  def test_devicefarm(self):
+    self.driver.get(
+            'http://docs.aws.amazon.com/devicefarm/latest/developerguide/welcome.html')
+    sleep(5)
+    screenshot_folder = os.getenv('SCREENSHOT_PATH', '/tmp')
+    self.driver.save_screenshot(screenshot_folder + '/devicefarm.png')
+
+  # later, make sure to end your WebDriver session:
+  def teardown_method(self, method):
+    self.driver.quit()
+
+# Start of script
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(DeviceFarmAppiumWebTests)
+    unittest.TextTestRunner(verbosity=2).run(suite)
